fcd05d065548c881b7a2ddb8f285bd538f571166
Liam.Li <398389303@qq.com>
2021-06-09 21:22:05 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import svg from './svgs/logo.svg'
 import PropTypes from 'prop-types'
-
+import { Button } from 'antd'
 function App(props) {
   return (
     <div>
       ls react app dsafds dsafdsa dsafdsaf dsafdsaf //
       <img src={svg} />
       {props.children}
+      <Button type="primary">test</Button>
     </div>
   )
 }
