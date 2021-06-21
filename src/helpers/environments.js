38c1a59aa7d702a1d56913b0a5feefcf66277f40
Liam Li <398389303@qq.com>
2021-06-21 20:26:29 +0800

    feat(a): test

--- src/helpers/environments.js
+++ src/helpers/environments.js
@@ -1,4 +1,4 @@
-import window from 'global/window';
+import window from 'global/window'
 
 // Filter by hostname (in order)
 //
@@ -7,25 +7,25 @@ import window from 'global/window';
 // 3. STAGE > ends with .dandythrust.com
 // 4. DEV > everything else
 
-let environment;
+let environment
 
-const hostnameEndsWith = (string) => {
+const hostnameEndsWith = string => {
   if (window.location && window.location.hostname) {
-    const { hostname } = window.location;
-    return hostname.substring(hostname.length - string.length) === string;
+    const { hostname } = window.location
+    return hostname.substring(hostname.length - string.length) === string
   }
 
-  return '';
-};
+  return ''
+}
 
 const isUat = () => {
   if (window.location && window.location.hostname) {
-    const { hostname } = window.location;
-    return hostname.includes('uat');
+    const { hostname } = window.location
+    return hostname.includes('uat')
   }
 
-  return false;
-};
+  return false
+}
 
 export const environments = {
   PROD: 'PROD',
@@ -34,43 +34,43 @@ export const environments = {
   UAT2: 'UAT2',
   UAT3: 'UAT3',
   DEV: 'DEV',
-};
+}
 
 export const getUatEnvironment = () => {
   if (isUat()) {
-    const { hostname } = window.location;
+    const { hostname } = window.location
     if (hostname.includes('uat1')) {
-      return environments.UAT1;
+      return environments.UAT1
     } else if (hostname.includes('uat2')) {
-      return environments.UAT2;
+      return environments.UAT2
     } else if (hostname.includes('uat3')) {
-      return environments.UAT3;
+      return environments.UAT3
     }
   }
 
-  return environments.UAT1;
-};
+  return environments.UAT1
+}
 
 export const clearCache = () => {
-  environment = null;
-};
+  environment = null
+}
 
 export const getEnvironment = () => {
   if (environment) {
-    return environment;
+    return environment
   }
 
   if (typeof window === 'undefined') {
-    environment = environments.PROD;
+    environment = environments.PROD
   } else if (hostnameEndsWith('student.com')) {
-    environment = environments.PROD;
+    environment = environments.PROD
   } else if (hostnameEndsWith('dandythrust.com') && isUat()) {
-    environment = getUatEnvironment();
+    environment = getUatEnvironment()
   } else if (hostnameEndsWith('dandythrust.com') && !isUat()) {
-    environment = environments.STAGE;
+    environment = environments.STAGE
   } else {
-    environment = environments.DEV;
+    environment = environments.DEV
   }
 
-  return environment;
-};
+  return environment
+}
